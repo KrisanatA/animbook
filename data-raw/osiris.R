26aa859a4fc14c30063ccd71ffe3ab1f1596e989
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-03-13 16:01:57 +1100

    the code needed to prepare the osiris dataset

---
+++ data-raw/osiris.R
@@ -0,0 +1,48 @@
+# The code needed to prepare the `osiris` dataset -------------------------
+
+
+# Library -----------------------------------------------------------------
+
+library(tidyverse)
+
+
+# Read the raw data -------------------------------------------------------
+
+raw_data <- read_csv("data-raw/osiris2021-sample1000.csv")
+
+
+# Prepare the data --------------------------------------------------------
+
+# filter the data with sales record in selected period (2009 - 2018)
+data <- raw_data |>
+  filter(year >= 2009 & year < 2019) |>
+  select(year, country, firmID, sales) |>
+  filter(!is.na(sales) & sales != 0)
+
+# pull the company name (unique)
+country <- data |>
+  select(firmID, country) |>
+  distinct()
+
+# complete the data using complete function for the missing year
+complete <- data |>
+  select(-country) |>
+  complete(year, firmID) |>
+  left_join(country)
+
+# rank the sales
+rank <- data |>
+  group_by(year) |>
+  mutate(rank = as.integer(rank(-sales)),
+         percentile = rank(-sales)/length(sales)) |>
+  ungroup()
+
+# join the table to get
+osiris <- complete |>
+  left_join(rank) |>
+  mutate_if(is.numeric, ~replace_na(., 0)) |>
+  relocate(country, .after = firmID)
+
+
+usethis::use_data(osiris, overwrite = TRUE)
+
