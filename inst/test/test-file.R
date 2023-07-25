b39f66b19245a560487dd6bc5f06d85135432a49
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-07-25 11:32:14 +1000

    adding toy dataset

--- inst/test/test-file.R
+++ inst/test/test-file.R
@@ -1,3 +1,34 @@
+library(tidyverse)
+
+
+# Toy dataset -------------------------------------------------------------
+
+set.seed(29803829)
+
+key <- 1:30
+rank_dbl <- as.double(runif(100, 1, 100))
+rank_int <- as.integer(runif(100, 1, 100))
+rank_fac <- as.factor(floor(runif(100, 1, 6)))
+year <- 2001:2010
+
+toy_dbl <- tibble(expand.grid(key, year),
+              rank = sample(rank_dbl, 300, replace = TRUE),
+              group = sample(c("apple", "samsung"), 300, replace = TRUE))
+names(toy_dbl) <- c("id", "year", "rank", "group")
+
+toy_int <- tibble(expand.grid(key, year),
+                  rank = sample(rank_int, 300, replace = TRUE),
+                  group = sample(c("apple", "samsung"), 300, replace = TRUE))
+names(toy_int) <- c("id", "year", "rank", "group")
+
+toy_fac <- tibble(expand.grid(key, year),
+                  rank = sample(rank_fac, 300, replace = TRUE),
+                  group = sample(c("apple", "samsung"), 300, replace = TRUE))
+names(toy_fac) <- c("id", "year", "rank", "group")
+
+
+# Osiris data -------------------------------------------------------------
+
 rank <- osiris |>
   group_by(year) |>
   mutate(rank = as.integer(rank(-sales)),
