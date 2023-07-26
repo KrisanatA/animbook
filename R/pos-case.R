845dfe204315516672e85213bc8c90ffc6b9546f
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-07-26 11:19:12 +1000

    separate the automatically position function from the prep_anim function

---
+++ R/pos-case.R
@@ -0,0 +1,34 @@
+pos_case <- function(n_group = 5) {
+
+  n <- length(n_group)
+
+  pos_list <- list()
+
+  # if (n > 1) {
+  #   for(i in 1:n) {
+  #     q <- 1 - (i * (1/n))
+  #     case <- paste0(".data$percentile > ", q, " ~ ", n_group[i])
+  #
+  #     pos_list[[i]] <- case
+  #   }
+  #
+  #   list <- rlang::parse_exprs(paste(pos_list))
+  #
+  #   return(list)
+  # }
+
+  # if the user supplied the number of group
+  if (n == 1) {
+    for(i in 1:n_group) {
+      q <- 1 - (i * (1/n_group))
+      case <- paste0(".data$percentile > ", round(q, 2), " ~ ", abs(i - n_group))
+
+      pos_list[[i]] <- case
+    }
+
+    list <- rlang::parse_exprs(paste(pos_list))
+
+    return(list)
+  }
+}
+
--- R/prep-anim.R
+++ R/prep-anim.R
@@ -4,21 +4,8 @@ prep_anim <- function(df, id = NULL, values = NULL, n_group = 5) {
   qvalues <- enquo(values)
   col_name <- as_label(qvalues)
 
-  pos_list <- list()
-
   # get the columns type
   type <- sapply(df, class)[[col_name]]
-
-  # automatically assign the pos to the values if the class of the column is numeric or integer
-  for(i in 1:n_group) {
-    n <- n_group
-    q <- 1 - (i * (1/n))
-    case <- paste0(".data$percentile > ", q, " ~ ", abs(i - 6))
-
-    pos_list[[i]] <- case
-    }
-
-  list <- rlang::parse_exprs(paste(pos_list, sep = ", "))
 
   # if the class of the values column is numeric or integer
   if (type == "numeric"|type == "integer") {
@@ -30,7 +17,7 @@ prep_anim <- function(df, id = NULL, values = NULL, n_group = 5) {
                       time = time + floor(runif(1, 10, 100)),
                       rank = as.integer(rank(-!!qvalues)),
                       percentile = rank(-!!qvalues)/length(!!qvalues),
-                      pos = case_when(!!!list),
+                      pos = case_when(!!!pos_case(n_group)),
                       pos = as.factor(pos)) |>
         dplyr::select(-c(rank, percentile)) |>
         dplyr::ungroup()
