23b87c4829ce3237d57d31dc18ad5dcceeea4492
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-07-26 16:43:06 +1000

    change the anim_plot structure, the function does not yet includes annotate and scale_colour

--- R/anim-plot.R
+++ R/anim-plot.R
@@ -1,27 +1,23 @@
-#' @import ggplot2
+#' @importFrom ggplot2 ggplot geom_point geom_hline annotate theme
 
-anim_plot <- function(df, x, y, id) {
+anim_plot <- function(data, id = NULL, values = NULL, x_axis = NULL, ...) {
 
-  data <- anim_data(df, {{id}}, {{y}})
+  qid <- enquo(id)
+  qvalues <- enquo(values)
+  qx_axis <- enquo(x_axis)
+
+  data <- prep_anim(data, id = !!qid, values = !!qvalues, ...)
+
+  hline <- unique(data$pos)
 
   anim <- data |>
-    ggplot2::ggplot() +
-    ggplot2::geom_point(aes(x = {{x}}, y = y, group = {{id}}, color = japan)) +
-    ggplot2::geom_hline(yintercept = 0:5, linewidth = 5.6, alpha = 0.1) +
-    ggplot2::annotate("text", x = 2007.5, y = 5, label = "Top 20%") +
-    ggplot2::annotate("text", x = 2007.5, y = 4, label = "21 - 40") +
-    ggplot2::annotate("text", x = 2007.5, y = 3, label = "41 - 60") +
-    ggplot2::annotate("text", x = 2007.5, y = 2, label = "61 - 80") +
-    ggplot2::annotate("text", x = 2007.5, y = 1, label = "81 - 100") +
-    ggplot2::annotate("text", x = 2007.5, y = 0, label = "Not listed") +
-    ggplot2::scale_colour_manual(name = "",
-                        breaks = c("Yes", "No"),
-                        labels = c("From Japan", "Not from Japan"),
-                        values = c("red", "blue")) +
-    ggplot2::scale_x_continuous(breaks = seq(2009, 2018, 1)) +
-    ggplot2::coord_cartesian(xlim = c(2009, 2018),
+    ggplot() +
+    geom_jitter(aes(x = !!qx_axis, y = pos, group = !!qid, color = group), height = 0.1) +
+    geom_hline(yintercept = hline, linewidth = 6, alpha = 0.1) +
+    scale_x_continuous(breaks = seq(min(year), max(year), 1)) +
+    coord_cartesian(xlim = c(min(year), max(year)),
                     clip = 'off') +
-    ggplot2::theme(aspect.ratio = 2/3,
+    theme(aspect.ratio = 2/3,
           panel.background = element_blank(),
           axis.title.y = element_blank(),
           axis.text.y = element_blank(),
@@ -29,6 +25,19 @@ anim_plot <- function(df, x, y, id) {
           axis.line.y = element_blank(),
           axis.title.x = element_blank(),
           axis.ticks.x = element_blank(),
-          plot.margin = margin(1, 1, 1, 4, "cm"),
+          plot.margin = margin(1, 1, 1, 1, "cm"),
           legend.position = "bottom")
+
+
+
+  #   ggplot2::annotate("text", x = 2007.5, y = 5, label = "Top 20%") +
+  #   ggplot2::annotate("text", x = 2007.5, y = 4, label = "21 - 40") +
+  #   ggplot2::annotate("text", x = 2007.5, y = 3, label = "41 - 60") +
+  #   ggplot2::annotate("text", x = 2007.5, y = 2, label = "61 - 80") +
+  #   ggplot2::annotate("text", x = 2007.5, y = 1, label = "81 - 100") +
+  #   ggplot2::annotate("text", x = 2007.5, y = 0, label = "Not listed") +
+  #   ggplot2::scale_colour_manual(name = "",
+  #                       breaks = c("Yes", "No"),
+  #                       labels = c("From Japan", "Not from Japan"),
+  #                       values = c("red", "blue"))
 }
