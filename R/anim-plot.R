885f7b6ac563aceee2345a98f524dfb919100cbd
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-07-26 21:13:46 +1000

    automatically format the x-axis

--- R/anim-plot.R
+++ R/anim-plot.R
@@ -1,34 +1,30 @@
 #' @importFrom ggplot2 ggplot geom_point geom_hline annotate theme
 
-anim_plot <- function(data, id = NULL, values = NULL, x_axis = NULL, ...) {
-
-  qid <- enquo(id)
-  qvalues <- enquo(values)
-  qx_axis <- enquo(x_axis)
-
-  data <- prep_anim(data, id = !!qid, values = !!qvalues, ...)
+anim_plot <- function(data, id = NULL, values = NULL, x_axis = NULL, color = NULL) {
 
   hline <- unique(data$pos)
 
+  qx <- enquo(x_axis)
+
+  x <- data[, as_label(qx)]
+
   anim <- data |>
     ggplot() +
-    geom_jitter(aes(x = !!qx_axis, y = pos, group = !!qid, color = group), height = 0.1) +
+    geom_jitter(aes(x = {{ x_axis }}, y = pos, group = {{ id }}, color = {{ color }}), height = 0.1) +
     geom_hline(yintercept = hline, linewidth = 6, alpha = 0.1) +
-    scale_x_continuous(breaks = seq(min(year), max(year), 1)) +
-    coord_cartesian(xlim = c(min(year), max(year)),
+    scale_x_continuous(breaks = seq(min(x), max(x), 1)) +
+    coord_cartesian(xlim = c(min(x), max(x)),
                     clip = 'off') +
     theme(aspect.ratio = 2/3,
-          panel.background = element_blank(),
-          axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.line.y = element_blank(),
-          axis.title.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          plot.margin = margin(1, 1, 1, 1, "cm"),
-          legend.position = "bottom")
-
-
+         panel.background = element_blank(),
+         axis.title.y = element_blank(),
+         axis.text.y = element_blank(),
+         axis.ticks.y = element_blank(),
+         axis.line.y = element_blank(),
+         axis.title.x = element_blank(),
+         axis.ticks.x = element_blank(),
+         plot.margin = margin(1, 1, 1, 1, "cm"),
+         legend.position = "bottom")
 
   #   ggplot2::annotate("text", x = 2007.5, y = 5, label = "Top 20%") +
   #   ggplot2::annotate("text", x = 2007.5, y = 4, label = "21 - 40") +
