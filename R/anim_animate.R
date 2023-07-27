47d9011c1c7656eb220aadc3abfdfb0085ceeebb
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-07-27 11:02:45 +1000

    adding gganimate package dependencies to the anim_animate function

--- R/anim_animate.R
+++ R/anim_animate.R
@@ -1,10 +1,12 @@
+#' @import gganimate
+
 anim_animate <- function(plot, ...) {
   params <- list(...)
 
   if (length(params) == 0) {
     return(
       plot +
-        gganimate::transition_time(time)
+        transition_time(time)
     )
   }
 
