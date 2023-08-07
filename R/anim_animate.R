843018ce09477fce0e9cf0c659e28045b96e2dc0
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-07 22:30:57 +1000

    trying to expand the animation using plotly (not working yet)

--- R/anim_animate.R
+++ R/anim_animate.R
@@ -1,18 +1,38 @@
 #' @import gganimate
 
-anim_animate <- function(plot, ...) {
+anim_animate <- function(plot, rendering = "gganimate", ...) {
   params <- list(...)
 
-  if (length(params) == 0) {
-    return(
-      plot +
-        transition_time(time)
-    )
+  rendering_choice <- c("plotly", "gganimate")
+
+  if (rendering %in% rendering_choice) {
+
+    if (rendering == "gganimate") {
+
+      if (length(params) == 0) {
+        return(
+          plot +
+            transition_time(frame)
+        )
+      }
+
+      if (length(params) > 0) {
+        return(
+          plot +
+            params)
+      }
+    }
+
+    # if (rendering == "plotly") {
+    #   return(
+    #       plotly::ggplotly(plot)
+    #   )
+    # }
   }
 
-  if (length(params) > 0) {
-    return(
-      plot +
-        params)
+  else {
+    rlang::abort("rendering argument can only be either plotly or gganimate")
   }
+
+
 }
