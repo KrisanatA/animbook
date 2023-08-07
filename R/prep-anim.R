8a183204f48efe2398aecc072a886044aa433099
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-07 11:35:02 +1000

    allow the function to assigned NA to 0

--- R/prep-anim.R
+++ R/prep-anim.R
@@ -2,9 +2,9 @@
 
 prep_anim <- function(data, id = NULL, values = NULL, time = NULL, ngroup = 5) {
 
-  qid <- enquo(id)
-  qvalues <- enquo(values)
-  qtime <- enquo(time)
+  qid <- rlang::enquo(id)
+  qvalues <- rlang::enquo(values)
+  qtime <- rlang::enquo(time)
 
   type <- sapply(data, class)[[as_label(qvalues)]]
 
@@ -17,11 +17,24 @@ prep_anim <- function(data, id = NULL, values = NULL, time = NULL, ngroup = 5) {
                frame = frame + floor(runif(1, 1, 100))) |>
         # split the values into groups using cut and quantile
         group_by(!!qtime) |>
-        mutate(qtile = cut(!!qvalues,
-                           stats::quantile(!!qvalues,
-                                           probs = seq(0, 1, 1/ngroup)),
-                           include.lowest = TRUE,
-                           labels = seq(1, ngroup, 1))) |>
+        # ranking the variable of interest
+        mutate(rank = as.integer(rank(!!qvalues)),
+               # if the values is NA then rank will also be NA
+               rank = ifelse(is.na(!!qvalues), NA, rank)) |>
+        ungroup() |>
+        # assign the y-axis values of the plot to each row based on their ranking quantile
+        mutate(
+          # using the cut function to assign the values to each row
+          qtile = cut(rank,
+                      stats::quantile(rank,
+                                      probs = seq(0, 1, 1/ngroup),
+                                      # do not count the NA values
+                                      na.rm = TRUE),
+                      include.lowest = TRUE,
+                      labels = rev(seq(1, ngroup, 1))),
+          # if the qtiles is NA then assign it to 0 otherwise converted the qtile from factor to integer
+          qtile = ifelse(is.na(qtile), 0, as.integer(levels(qtile)[qtile]))
+          ) |>
         ungroup()
     )
   }
