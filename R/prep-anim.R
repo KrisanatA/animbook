d03e863cf0882bd25c73bfe297b95153fb164d85
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-09 15:11:24 +1000

    prep_anim function only return columns that will be use

--- R/prep-anim.R
+++ R/prep-anim.R
@@ -1,3 +1,12 @@
+#'Prep the data structure for plot function
+#'
+#'This function prepared the data into the format the [anim_plot()] required.
+#'
+#'@param data The data frame or a tibble which contains the information of interested.
+#'@param id The name of the column that uniquely identified the elements of interested
+#'@param values The name of the column within the data frame that the user
+#'
+#'
 #'@importFrom dplyr group_by arrange mutate select ungroup
 #'@export
 
@@ -7,39 +16,42 @@ prep_anim <- function(data, id = NULL, values = NULL, time = NULL, ngroup = 5) {
   qvalues <- rlang::enquo(values)
   qtime <- rlang::enquo(time)
 
-  type <- sapply(data, class)[[as_label(qvalues)]]
+  type <- sapply(data, class)[[rlang::as_label(qvalues)]]
 
   stopifnot("This function only accepted numerical values" = type %in% c("numeric", "integer"))
 
-  return(
-    data |>
-      # assigned the frame for each row which will be uses for the transition
-      arrange(!!qid, !!qtime) |>
-      group_by(!!qid) |>
-      mutate(frame = row_number(),
-             frame = frame + floor(runif(1, 1, 100))) |>
-      ungroup() |>
-      # split the values into groups using cut and quantile
-      group_by(!!qtime) |>
-      # ranking the variable of interest
-      mutate(rank = as.integer(rank(!!qvalues)),
-             # if the values is NA then rank will also be NA
-             rank = ifelse(is.na(!!qvalues), NA, rank)) |>
-      ungroup() |>
-      # assign the y-axis values of the plot to each row based on their ranking quantile
-      mutate(
-        # using the cut function to assign the values to each row
-        qtile = cut(rank,
-                    stats::quantile(rank,
-                                    probs = seq(0, 1, 1/ngroup),
-                                    # do not count the NA values
-                                    na.rm = TRUE),
-                    include.lowest = TRUE,
-                    labels = rev(seq(1, ngroup, 1))),
-        # if the qtiles is NA then assign it to 0 otherwise converted the qtile from factor to integer
-        qtile = ifelse(is.na(qtile), 0, as.integer(levels(qtile)[qtile]))
+  book <- data |>
+    arrange(!!qid, !!qtime) |>
+    group_by(!!qid) |>
+    mutate(
+      frame = dplyr::row_number(),
+      frame = frame + floor(runif(1, 1, 100))
       ) |>
-      ungroup()
-  )
+    ungroup() |>
+    # split the values into groups using cut and quantile
+    group_by(!!qtime) |>
+    # ranking the variable of interest
+    mutate(
+      rank = as.integer(rank(!!qvalues)),
+      # if the values is NA then rank will also be NA
+      rank = ifelse(is.na(!!qvalues), NA, rank),
+      .keep = "unused") |>
+    ungroup() |>
+    # assign the y-axis values of the plot to each row based on their ranking quantile
+    mutate(
+      # using the cut function to assign the values to each row
+      qtile = cut(rank,
+                  stats::quantile(rank,
+                                  probs = seq(0, 1, 1/ngroup),
+                                  # do not count the NA values
+                                  na.rm = TRUE),
+                  include.lowest = TRUE,
+                  labels = rev(seq(1, ngroup, 1))),
+      # if the qtiles is NA then assign it to 0 otherwise converted the qtile from factor to integer
+      qtile = ifelse(is.na(qtile), 0, as.integer(levels(qtile)[qtile])),
+      .keep = "unused") |>
+    ungroup()
+
+  return(book)
 
 }
