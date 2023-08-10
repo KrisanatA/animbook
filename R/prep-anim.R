6f7a12f3fa5fcf09ae449e1a0e70c942eb2f7c26
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-10 15:42:33 +1000

    add time_dependent argument and check ngroup number with the number of unique id

--- R/prep-anim.R
+++ R/prep-anim.R
@@ -4,10 +4,12 @@
 #'
 #'@param data The data frame or a tibble which contains the information of interested.
 #'@param id Name of the column that uniquely identified the elements of interested
-#'@param values Name of the column of interested, must be a numeric column.
+#'@param values Name of the column to be rank and assigned the group, must be a numeric column.
 #'@param time Name of the columns that will be used as time variable.
 #'@param ngroup Number of group the user want to split the values into (excluded
 #'the NA group which is the additional group).
+#'@param time_dependent Default for this function is FALSE, this means each id
+#'will enter the animation at the different time
 #'
 #'@return Return a tibble with additional frame and qtile columns (excluded the
 #'values columns which been used to calculated the qtile column)
@@ -22,7 +24,8 @@ prep_anim <- function(data,
                       id = NULL,
                       values = NULL,
                       time = NULL,
-                      ngroup = 5) {
+                      ngroup = 5,
+                      time_dependent = FALSE) {
 
   qid <- rlang::enquo(id)
   qvalues <- rlang::enquo(values)
@@ -30,36 +33,58 @@ prep_anim <- function(data,
 
   type <- sapply(data, class)[[rlang::as_label(qvalues)]]
 
+  n <- unique(data[, as_label(qid)])
+
   stopifnot("This function only accepted numerical values" = type %in% c("numeric", "integer"))
 
-  book <- data |>
-    arrange(!!qid, !!qtime) |>
-    group_by(!!qid) |>
-    mutate(
-      frame = dplyr::row_number(),
-      frame = frame + floor(runif(1, 1, 50))
+  if (ngroup > length(n)) {
+    ngroup <- length(n)
+  }
+
+
+# assign the frame --------------------------------------------------------
+
+  if (time_dependent == FALSE) {
+    data_frame <- data |>
+      arrange(!!qid, !!qtime) |>
+      group_by(!!qid) |>
+      mutate(
+        frame = dplyr::row_numer(),
+        # will need to make the size of the runif be automatically
+        frame = frame + floor(runif(1, 1, 50))
       ) |>
-    ungroup() |>
-    # split the values into groups using cut and quantile
+      ungroup()
+  }
+
+  if (time_dependent == TRUE) {
+    data_frame <- data |>
+      arrange(!!qid, !!qtime) |>
+      group_by(!!qid) |>
+      mutate(
+        frame = dplyr::row_number()
+      ) |>
+      ungroup()
+  }
+
+
+# assign the qtile --------------------------------------------------------
+
+  book <- data_frame |>
     group_by(!!qtime) |>
     # ranking the variable of interest
     mutate(
       rank = as.integer(rank(!!qvalues)),
-      # if the values is NA then rank will also be NA
       rank = ifelse(is.na(!!qvalues), NA, rank),
       .keep = "unused") |>
     ungroup() |>
     # assign the y-axis values of the plot to each row based on their ranking quantile
     mutate(
-      # using the cut function to assign the values to each row
       qtile = cut(rank,
                   stats::quantile(rank,
                                   probs = seq(0, 1, 1/ngroup),
-                                  # do not count the NA values
                                   na.rm = TRUE),
                   include.lowest = TRUE,
                   labels = rev(seq(1, ngroup, 1))),
-      # if the qtiles is NA then assign it to 0 otherwise converted the qtile from factor to integer
       qtile = ifelse(is.na(qtile), 0, as.integer(levels(qtile)[qtile])),
       .keep = "unused") |>
     ungroup()
