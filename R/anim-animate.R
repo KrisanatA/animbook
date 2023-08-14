52af5e8ecfda996c13140d1f7b9499a8cb5856d7
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-14 16:41:05 +1000

    adding plotly dependencies and document the function

--- R/anim-animate.R
+++ R/anim-animate.R
@@ -1,4 +1,25 @@
-#'@import gganimate
+#'Modified the ggplot object
+#'
+#'This function by default will modified the ggplot object before the user can
+#'pass it to the rendering of choice.
+#'
+#'@param plot ggplot object
+#'@param modify Default is FALSE which supplied the function needed for the rendering
+#'package.
+#'
+#'@return A gganimate object if the rendering is gganimate or a ggplot object if
+#'the rendering is plotly.
+#'
+#'@examples
+#'data <- prep_anim(data = osiris, id = firmID, values = sales, time = year, ngroup = 5)
+#'
+#'label = c("Top 20%", "21-40", "41-60", "61-80", "81-100", "Not listed", "test")
+#'
+#'p <- anim_plot(data = data, id = firmID, time = year, color = japan, label = label)
+#'
+#'p2 <- anim_animate(p)
+#'
+#'@import gganimate plotly
 #'@export
 
 anim_animate <- function(plot, modify = FALSE) {
