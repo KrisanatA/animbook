830a05fc8c842a95dcc85381add5df8ad5e962d8
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-08-18 15:07:15 +1000

    documenting the aes data

--- R/data.R
+++ R/data.R
@@ -16,3 +16,24 @@
 #'@source This dataset is from the following; Bureau van Dijk
 #'\url{https://www.bvdinfo.com/en-gb/our-products/data/international/osiris}.
 "osiris"
+
+
+
+#'Australian election study data
+#'
+#'The aes dataset contains the answer of the surveys which is done in 2019 for the election.
+#'This dataset only includes the id, year, party, gender from the the full survey data. The year
+#'column come from the transformations of two different question to see whether the voter voted the
+#'same party in 2016 and 2019 or not and if not who did they voted for before.
+#'
+#'@format A data frame with 1,468 rows and 4 variables
+#'\describe{
+#'  \item{id}{The id of the respondent}
+#'  \item{year}{Year}
+#'  \item{party}{Party that the respondent vote for in the House of Representatives}
+#'  \item{gender}{Gender of the respondent}
+#'}
+#'
+#'@source This dataset is from the following; Australian Election Study
+#'\url{https://dataverse.ada.edu.au/file.xhtml?fileId=18013&version=3.0}
+"aes"
