a7d49c2fb6403a7d652adf43cb90b2450e5bb1c1
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2023-09-18 22:00:07 +1000

    add funtion list file

---
+++ inst/test/function-list.R
@@ -0,0 +1,37 @@
+library(animbook)
+
+# osiris ------------------------------------------------------------------
+
+label <- c("Top20", "20-40", "40-60", "60-80", "80-100", "Not listed")
+
+osiris1 <- anim_prep(data = osiris, id = ID, values = sales, time = year, label = label,
+                     ngroup = 5L, color = japan)
+
+osiris2 <- anim_prep(data = osiris, id = ID, values = sales, time = year, label = label,
+                     ngroup = 5L, group_scaling = country, color = japan)
+
+osiris3 <- anim_prep(data = osiris, id = ID, values = sales, time = year,
+                     ngroup = 5L, color = japan, scaling = "absolute")
+
+osiris4 <- anim_prep(data = osiris, id = ID, values = sales, time = year,
+                     ngroup = 5L, group_scaling = country, color = japan, scaling = "absolute")
+
+
+
+
+# aeles -------------------------------------------------------------------
+
+order <- c("liberal", "not vote", "greens", "other", "national", "labor")
+
+object <- anim_prep_cat(data = aeles, id = id, values = party, time = year,
+                        order = NULL, color = gender, time_dependent = TRUE,
+                        runif_min = 1, runif_max = 50)
+
+# subset can be either "top", "bottom" or values in the data
+# many_one not working yet
+
+p <- wallaby_plot(object = object, palette = RColorBrewer::brewer.pal(9, "Set1"),
+                  rendering = "ggplot", subset = "top", relation = "one_many")
+
+p2 <- anim_aniate(p)
+
