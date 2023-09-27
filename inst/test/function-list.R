efa2be717ecb298ee3457e936f389cb29ba29ee8
dicook <visnut@gmail.com>
2023-09-27 16:39:33 +1000

    more example code

--- inst/test/function-list.R
+++ inst/test/function-list.R
@@ -4,22 +4,60 @@ library(animbook)
 
 label <- c("Top20", "20-40", "40-60", "60-80", "80-100", "Not listed")
 
-osiris1 <- anim_prep(data = osiris, id = ID, values = sales, time = year, label = label,
-                     ngroup = 5L, color = japan)
+osiris1 <- anim_prep(data = osiris,
+                     id = ID,
+                     values = sales,
+                     time = year,
+                     label = label,
+                     ngroup = 5L,
+                     color = japan)
 
 osiris2 <- anim_prep(data = osiris, id = ID, values = sales, time = year, label = label,
                      ngroup = 5L, group_scaling = country, color = japan)
 
-osiris3 <- anim_prep(data = osiris, id = ID, values = sales, time = year,
-                     ngroup = 5L, color = japan, scaling = "absolute")
+osiris3 <- anim_prep(data = osiris,
+                     id = ID,
+                     values = sales,
+                     time = year,
+                     ngroup = 5L,
+                     color = japan,
+                     scaling = "absolute",
+                     time_dependent = FALSE)
 
 osiris4 <- anim_prep(data = osiris, id = ID, values = sales, time = year,
                      ngroup = 5L, group_scaling = country, color = japan, scaling = "absolute")
 
-osiris_p <- wallaby_plot(object = osiris3, subset = "top")
+osiris_p <- wallaby_plot(object = osiris3,
+                         palette = RColorBrewer::brewer.pal(9, "Set1"),
+                         subset = "top")
 
+osiris_anim <- anim_animate(osiris_p)
 
+gganimate::animate(osiris_anim)
 
+plotly::ggplotly(osiris_anim)
+
+osiris_us <- osiris %>%
+  mutate(usa = ifelse(country == "US", "usa", "others")) %>%
+  mutate(lsales = log(sales))
+
+osiris5 <- anim_prep(data = osiris_us,
+                     id = ID,
+                     values = lsales,
+                     time = year,
+                     ngroup = 5L,
+                     color = usa,
+                     scaling = "absolute",
+                     group_scaling = country,
+                     time_dependent = FALSE)
+
+osiris_p <- wallaby_plot(object = osiris5,
+                         palette = RColorBrewer::brewer.pal(8, "Paired"),
+                         subset = "top")
+
+osiris_anim <- anim_animate(osiris_p)
+
+gganimate::animate(osiris_anim)
 
 # aeles -------------------------------------------------------------------
 
@@ -40,3 +78,5 @@ p2 <- anim_animate(p)
 gganimate::animate(p2)
 
 plotly::ggplotly(p2)
+
+
